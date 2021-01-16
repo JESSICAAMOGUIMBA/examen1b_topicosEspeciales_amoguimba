84e62f404df2167092af51ca6377b0d2effdccca
JESSICAAMOGUIMBA <jessica.amoguimba@epn.edu.ec>
2021-01-15 19:58:15 -0500

    Examen-Login-Registro

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC2cS2BmgzsEc7tsgvTIpbl8o87uofZypo",
+    authDomain: "miprimerlogin-9fe8d.firebaseapp.com",
+    databaseURL: "https://miprimerlogin-9fe8d-default-rtdb.firebaseio.com",
+    projectId: "miprimerlogin-9fe8d",
+    storageBucket: "miprimerlogin-9fe8d.appspot.com",
+    messagingSenderId: "161704047626",
+    appId: "1:161704047626:web:213c9ce218d0d012dc9bf3",
+    measurementId: "G-SQ9GSYLJ5W"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC2cS2BmgzsEc7tsgvTIpbl8o87uofZypo",
+    authDomain: "miprimerlogin-9fe8d.firebaseapp.com",
+    databaseURL: "https://miprimerlogin-9fe8d-default-rtdb.firebaseio.com",
+    projectId: "miprimerlogin-9fe8d",
+    storageBucket: "miprimerlogin-9fe8d.appspot.com",
+    messagingSenderId: "161704047626",
+    appId: "1:161704047626:web:213c9ce218d0d012dc9bf3",
+    measurementId: "G-SQ9GSYLJ5W"
+  }
 };
 
 /*
